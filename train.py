cda1f729428cc3a89186c8b48fdb9ea4cf3cb617
romainducrocq <romain.g.ducrocq@gmail.com>
2021-05-07 16:43:36 +0200

    eps min to 0.01

--- train.py
+++ train.py
@@ -101,7 +101,7 @@ if __name__ == "__main__":
     parser.add_argument('-lr', type=float, default=0.00025, help='Learning rate')
     parser.add_argument('-gamma', type=float, default=0.99, help='Discount factor')
     parser.add_argument('-eps_start', type=float, default=1., help='Epsilon start')
-    parser.add_argument('-eps_min', type=float, default=0.1, help='Epsilon min')
+    parser.add_argument('-eps_min', type=float, default=0.01, help='Epsilon min')
     parser.add_argument('-eps_dec', type=float, default=1e6, help='Epsilon decay')
     parser.add_argument('-bs', type=int, default=32, help='Batch size')
     parser.add_argument('-min_mem', type=int, default=500000, help='Replay memory buffer min size')

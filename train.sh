a0701183a774bb11e0def78721f996cc902c454c
romainducrocq <romain.g.ducrocq@gmail.com>
2021-05-11 15:45:37 +0200

    fix script

--- train.sh
+++ train.sh
@@ -6,11 +6,9 @@ while true; do
 done
 '
 
-
-TOTAL_STEPS=2000000
-python3 train.py -lr 0.01 -max_total_steps $TOTAL_STEPS
-python3 train.py -lr 0.001 -max_total_steps $TOTAL_STEPS
-python3 train.py -lr 0.0001 -max_total_steps $TOTAL_STEPS
-python3 train.py -lr 0.00001 -max_total_steps $TOTAL_STEPS
+python3 train.py -lr 0.01 -max_total_steps 2000000
+python3 train.py -lr 0.001 -max_total_steps 2000000
+python3 train.py -lr 0.0001 -max_total_steps 2000000
+python3 train.py -lr 0.00001 -max_total_steps 2000000
 
 exit

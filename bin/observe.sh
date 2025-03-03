784d339387e99254084c8d15233e0184f02a7d3c
romainducrocq <romain.g.ducrocq@gmail.com>
2025-03-03 11:28:00 +0100

    update scripts

--- bin/observe.sh
+++ bin/observe.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/bash
 
-function run () {
-
-python3 observe.py -d save/PerDuelingDoubleDQNAgent_lr5e-05_model.pack -max_s 500
-
-}
-
-cd ..
+cd ../
 
 source venv/bin/activate
-
-run
-
+if [ -z "${1}" ]; then
+    python3 observe.py -d save/PerDuelingDoubleDQNAgent_lr5e-05_model.pack -max_s 500
+else
+    python3 observe.py "${@}"
+fi
 deactivate
 
 exit
